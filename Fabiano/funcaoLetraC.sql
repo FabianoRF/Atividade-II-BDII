66e583e34699a87ba4c430827281842e0324122e
FabianoRF <freis884@gmail.com>
2020-06-29 14:26:45 -0300

    letra c

--- Fabiano/funcaoLetraC.sql
+++ Fabiano/funcaoLetraC.sql
@@ -1,5 +1,25 @@
 DELIMITER $$
 
-    CREATE FUNCTION retornaDiaDaSemana(numDia INT) RETURNS INT
-    
+    CREATE FUNCTION tranformaEmIntervalo( hora_entrega TIME ) 
+    RETURNS varchar(20)
+
+    BEGIN
+        DECLARE hora varchar(20);
+        
+        IF (hora_entrega < '20:00:00') THEN
+        SET hora = 'Inicio';
+        
+
+        ELSEIF (hora_entrega>='20:00:00' AND hora_entrega<'22:00:00') THEN
+        SET hora = 'Pico';
+
+        ELSE
+        SET hora = 'Pico';
+        END IF;
+
+        RETURN hora;
+    END;
 $$
+
+
+SELECT transformaEmIntervalo(pedidos_full.hora_pedido) from pedidos_full
